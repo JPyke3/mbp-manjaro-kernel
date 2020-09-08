415c883571ac17677f7703b9ef0833402aa1bad2
JPyke3 <pyke.jacob1@gmail.com>
2020-09-09 07:30:29 +1000

    Removed Dockerfile

--- docker-commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Import GNUPG key for signing the package
-gpg --import --batch --passphrase $GPG_PASSPHRASE <(echo -e $GPG_KEY);
-
-# Make the package and sign it
-makepkg -s --sign
